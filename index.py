18ad69e41b9ec0fd99ed3ada7b7b8c4819937f9b
[tirthasubedi] <[subedi.tirtha@gmail.com]>
2019-09-07 16:23:31 -0400

    Displaying template

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
 #MorseLaw Search engin
-from flask import Flask
+from flask import Flask, escape, request, render_template
 
 app=Flask(__name__)
 
-from app import routes
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html', name=name)
+    # name = request.args.get("name", "World")
+    # return f'Hello, {escape(name)}!'
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
